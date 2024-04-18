cbb5da0f88cfab94795c508f5ae3660a57ecd2e6
Job Doesburg <job.doesburg@gmail.com>
2024-04-18 23:06:02 +0200

    Better params

--- tests/energy.rs
+++ tests/energy.rs
@@ -53,10 +53,10 @@ fn start_transcryptor(i:usize, s_from: ScalarNonZero, s_to: ScalarNonZero, k_fro
 
 #[test]
 fn energy_transcrypt() {
-    let iterations = 100;
+    let iterations = 50;
     let rest_before_measure = 2;
     let n_max = 5; // number of tiers
-    let m_exp_max = 6;
+    let m_exp_max = 4;
 
     let mut rng = OsRng;
 
@@ -93,7 +93,7 @@ fn energy_transcrypt() {
         for exp in 0..m_exp_max {
             let m = 10u64.pow(exp as u32) as usize;
 
-            eprintln!("\nBenchmarking with {} tiers and {} messages", n, m);
+            eprintln!("\nBenchmarking PEP with {} tiers and {} messages", n, m);
 
             sleep(std::time::Duration::from_secs(rest_before_measure));
             let t_before = SystemTime::now();
@@ -137,99 +137,110 @@ fn energy_transcrypt() {
 
 
 
+fn tunnel_handle(bytes: Vec<u8>, server_state: Rc<RefCell<ServerState>>) -> Result<Response<BoxedBody>, hyper::http::Error> {
+    let value = ElGamal::decode(&bytes).unwrap();
 
+    let state = server_state.borrow();
 
+    let result = value.encode().to_vec();
+    Ok(Response::new(box_body(BodyVec::from(result))))
+}
 
-//
-// fn tunnel_handler(bytes: Vec<u8>, _: Rc<RefCell<ServerState>>) -> Result<Response<BoxedBody>, hyper::http::Error> {
-//     let value = ElGamal::decode(&bytes).unwrap();
-//
-//     let result = value.encode().to_vec();
-//     Ok(Response::new(box_body(BodyVec::from(result))))
-// }
-//
-// fn start_tunnel(i:usize) -> std::thread::JoinHandle<()> {
-//     std::thread::spawn(move || {
-//         let rt = tokio::runtime::Builder::new_multi_thread()
-//             .thread_name("background")
-//             .worker_threads(std::thread::available_parallelism().map(|x| x.get()).unwrap_or(4))
-//             .enable_all()
-//             .build()
-//             .expect("build runtime");
-//
-//         let local = Box::new(tokio::task::LocalSet::new());
-//         let local : &'static tokio::task::LocalSet = Box::leak(local);
-//
-//         let state = ServerState { s_from: ScalarNonZero::one(), s_to: ScalarNonZero::one(), k_from: ScalarNonZero::one(), k_to: ScalarNonZero::one()};
-//         local.block_on(&rt, webserver((3330 + i) as u16, tunnel_handler, state));
-//     })
-// }
-//
-// fn no_transcrypt(n: usize, m: usize, iterations: usize, rest_before_measure: u64) -> (f64, f64) {
-//     let mut rng = OsRng;
-//
-//     // system params
-//     let y = ScalarNonZero::random(&mut rng); // global private
-//     let gy = y * G; // global public
-//
-//     // random message
-//     let data = GroupElement::random(&mut rng);
-//
-//     // factors
-//     let tiers = Vec::from_iter((0..n).map(|x| start_tunnel(x)));
-//
-//     // wait for webserver to start
-//     std::thread::sleep(std::time::Duration::from_secs(1));
-//
-//     let sender = get_agent();
-//
-//     // START BENCHMARK
-//     sleep(std::time::Duration::from_secs(rest_before_measure));
-//     let t_before = SystemTime::now();
-//     let before = get_ina();
-//
-//     for _ in 0.. iterations {
-//         for _ in 0..m {
-//             let mut value = encrypt(&data, &(gy), &mut OsRng); // initial encryption
-//
-//             // transcryption
-//             for i in 0..n {
-//                 let bytes = value.encode();
-//                 let response = sender.post(&format!("https://127.0.0.1:{}", 3330 + i)).send_bytes(&bytes[..]).unwrap();
-//                 let mut body = Vec::new();
-//                 response.into_reader().read_to_end(&mut body).unwrap();
-//                 value = ElGamal::decode(&body).unwrap();
-//             }
-//
-//             let decrypted = decrypt(&value, &(y)); // final decryption
-//             debug_assert_eq!(data, decrypted);
-//         }
-//     }
-//     // END BENCHMARK
-//     let after = get_ina();
-//
-//     let t_after = SystemTime::now();
-//
-//     let time_elapsed = t_after.duration_since(t_before).unwrap().as_secs_f64();
-//     if before.is_none() || after.is_none() {
-//         return (0.0, 0.0);
-//     }
-//     let energy_used = after.unwrap() - before.unwrap();
-//     (energy_used, time_elapsed)
-// }
-//
-// #[test]
-// fn energy_no_transcrypt() {
-//     let iterations = 10000;
-//     let rest_before_measure = 2;
-//     let n = 3; // number of tiers
-//     let m = 1;
-//
-//     let num_messages = 10u64.pow(m as u32) as usize;
-//     eprintln!("\nBenchmarking with {} tiers and {} messages", n, num_messages);
-//
-//     let (energy_used, time_elapsed) = no_transcrypt(n, num_messages, iterations, rest_before_measure);
-//     eprintln!("Time elapsed: {} s", time_elapsed);
-//     eprintln!("Energy used: {} J", energy_used);
-// }
-//
+fn start_tunnel(i:usize, s_from: ScalarNonZero, s_to: ScalarNonZero, k_from: ScalarNonZero, k_to: ScalarNonZero) -> std::thread::JoinHandle<()> {
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .thread_name("background")
+            .worker_threads(std::thread::available_parallelism().map(|x| x.get()).unwrap_or(4))
+            .enable_all()
+            .build()
+            .expect("build runtime");
+
+        let local = Box::new(tokio::task::LocalSet::new());
+        let local : &'static tokio::task::LocalSet = Box::leak(local);
+
+        let state = ServerState { s_from, s_to, k_from, k_to };
+        local.block_on(&rt, webserver((4440 + i) as u16, crate::tunnel_handle, state));
+    })
+}
+
+#[test]
+fn energy_tunnel() {
+    let iterations = 50;
+    let rest_before_measure = 2;
+    let n_max = 5; // number of tiers
+    let m_exp_max = 4;
+
+    let mut rng = OsRng;
+
+    // system params
+    let y = ScalarNonZero::random(&mut rng); // global private
+    let gy = y * G; // global public
+
+    // random message
+    let data = GroupElement::random(&mut rng);
+
+    // factors
+    let s_from_s = Vec::from_iter((0..n_max).map(|_| ScalarNonZero::random(&mut rng)));
+    let s_to_s = Vec::from_iter((0..n_max).map(|_| ScalarNonZero::random(&mut rng)));
+    let k_from_s = Vec::from_iter((0..n_max).map(|_| ScalarNonZero::random(&mut rng)));
+    let k_to_s = Vec::from_iter((0..n_max).map(|_| ScalarNonZero::random(&mut rng)));
+
+    let k_from = k_from_s.iter().fold(ScalarNonZero::one(), |acc, k| acc * k);
+    let k_to = k_to_s.iter().fold(ScalarNonZero::one(), |acc, k| acc * k);
+
+    let s_from = s_from_s.iter().fold(ScalarNonZero::one(), |acc, s| acc * s);
+    let s_to = s_to_s.iter().fold(ScalarNonZero::one(), |acc, s| acc * s);
+
+    let s = s_from.invert() * s_to;
+
+    let transcryptors = Vec::from_iter((0..n_max).map(|x| crate::start_tunnel(x, s_from_s[x], s_to_s[x], k_from_s[x], k_to_s[x])));
+
+    // wait for webserver to start
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    let sender = get_agent();
+
+    // START BENCHMARK
+    for n in 1..=n_max {
+        for exp in 0..m_exp_max {
+            let m = 10u64.pow(exp as u32) as usize;
+
+            eprintln!("\nBenchmarking NO PEP with {} tiers and {} messages", n, m);
+
+            sleep(std::time::Duration::from_secs(rest_before_measure));
+            let t_before = SystemTime::now();
+            let before = get_ina();
+
+            for _ in 0..iterations {
+                for _ in 0..m {
+                    let mut value = encrypt(&data, &(k_from * gy), &mut OsRng); // initial encryption
+
+                    // transcryption
+                    for i in 0..n {
+                        let bytes = value.encode();
+                        let response = sender.post(&format!("https://127.0.0.1:{}", 4440 + i)).send_bytes(&bytes[..]).unwrap();
+                        let mut body = Vec::new();
+                        response.into_reader().read_to_end(&mut body).unwrap();
+                        value = ElGamal::decode(&body).unwrap();
+                    }
+
+                    let decrypted = decrypt(&value, &(k_to * y)); // final decryption
+                    // debug_assert_eq!(s * data, decrypted); This doesnt hold anymore
+                }
+            }
+            // END BENCHMARK
+            let after = get_ina();
+
+            let t_after = SystemTime::now();
+
+            let time_elapsed = t_after.duration_since(t_before).unwrap().as_secs_f64();
+            if before.is_none() || after.is_none() {
+                continue;
+            }
+            let energy_used = after.unwrap() - before.unwrap();
+
+            eprintln!("Time elapsed: {} s", time_elapsed);
+            eprintln!("Energy used: {} J", energy_used);
+        }
+    }
+}
