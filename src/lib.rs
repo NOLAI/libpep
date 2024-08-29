4337ace617d3309657c875dc107333c031b7ba6d
Job Doesburg <job.doesburg@gmail.com>
2024-08-29 15:55:03 +0200

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,10 @@ pub mod distributed;
 pub mod utils;
 
 #[cfg(feature = "wasm")]
-pub mod wasm {
-    pub mod arithmetic;
-    pub mod elgamal;
-    pub mod primitives;
-    pub mod high_level;
-    pub mod distributed;
+mod wasm {
+    mod arithmetic;
+    mod elgamal;
+    mod primitives;
+    mod high_level;
+    mod distributed;
 }
