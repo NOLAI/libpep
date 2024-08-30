ae81f138b4544370d93ac47e265eaa2e016eb4cc
Job Doesburg <job.doesburg@gmail.com>
2024-08-30 12:07:57 +0200

    Refactor for readability

--- tests/high_level.rs
+++ tests/high_level.rs
@@ -84,8 +84,8 @@ fn test_proved() {
     let (session1_public, session1_secret) = make_session_keys(&global_secret, &enc_context1, &enc_secret);
     let (_session2_public, session2_secret) = make_session_keys(&global_secret, &enc_context2, &enc_secret);
 
-    let pseudo = Pseudonym::random();
-    let enc_pseudo = encrypt_pseudonym(&pseudo, &session1_public);
+    let pseudo_1 = Pseudonym::random();
+    let enc_pseudo_1 = encrypt_pseudonym(&pseudo_1, &session1_public);
 
     let data = DataPoint::new(GroupElement::random(&mut OsRng));
     let enc_data = encrypt_data(&data, &session1_public);
@@ -96,24 +96,24 @@ fn test_proved() {
     assert!(rekeyed_dec.is_some());
     assert_eq!(data, rekeyed_dec.unwrap());
 
-    let pseudonymized = proved_pseudonymize(&enc_pseudo, &pseudo_context1, &pseudo_context2, &enc_context1, &enc_context2, &pseudo_secret, &enc_secret);
+    let pseudonymized = proved_pseudonymize(&enc_pseudo_1, &pseudo_context1, &pseudo_context2, &enc_context1, &enc_context2, &pseudo_secret, &enc_secret);
 
-    let pseudonymized_dec = proved_decrypt_pseudonym(&pseudonymized, &enc_pseudo, &session2_secret, &pseudo_verifiers1, &pseudo_verifiers2, &rekey_verifiers1, &rekey_verifiers2);
+    let pseudonymized_dec = proved_decrypt_pseudonym(&pseudonymized, &enc_pseudo_1, &session2_secret, &pseudo_verifiers1, &pseudo_verifiers2, &rekey_verifiers1, &rekey_verifiers2);
 
     assert!(pseudonymized_dec.is_some());
-    assert_ne!(pseudo, pseudonymized_dec.unwrap());
+    assert_ne!(pseudo_1, pseudonymized_dec.unwrap());
 
-    let enc_pseudo_2 = pseudonymized.reconstruct(&enc_pseudo, &pseudo_verifiers1, &pseudo_verifiers2, &rekey_verifiers1, &rekey_verifiers2);
+    let enc_pseudo_2 = pseudonymized.reconstruct(&enc_pseudo_1, &pseudo_verifiers1, &pseudo_verifiers2, &rekey_verifiers1, &rekey_verifiers2);
     let dec_pseudo_2 = decrypt_pseudonym(&enc_pseudo_2.unwrap(), &session2_secret);
     assert_eq!(pseudonymized_dec.unwrap(), dec_pseudo_2);
 
-    let non_proved = pseudonymize(&enc_pseudo, &pseudo_context1, &pseudo_context2, &enc_context1, &enc_context2, &pseudo_secret, &enc_secret);
+    let non_proved = pseudonymize(&enc_pseudo_1, &pseudo_context1, &pseudo_context2, &enc_context1, &enc_context2, &pseudo_secret, &enc_secret);
     let non_proved_dec = decrypt_pseudonym(&non_proved, &session2_secret);
     assert_eq!(pseudonymized_dec.unwrap(), non_proved_dec);
 
-    let rev_pseudonymized = proved_re_pseudonymize(&pseudonymized, &enc_pseudo, &pseudo_context2, &pseudo_context1, &enc_context2, &enc_context1, &pseudo_secret, &enc_secret, &pseudo_verifiers1, &pseudo_verifiers2, &rekey_verifiers1, &rekey_verifiers2);
+    let rev_pseudonymized = proved_re_pseudonymize(&pseudonymized, &enc_pseudo_1, &pseudo_context2, &pseudo_context1, &enc_context2, &enc_context1, &pseudo_secret, &enc_secret, &pseudo_verifiers1, &pseudo_verifiers2, &rekey_verifiers1, &rekey_verifiers2);
     let rev_pseudonymized_dec = proved_decrypt_pseudonym(&rev_pseudonymized.unwrap(), &enc_pseudo_2.unwrap(), &session1_secret, &pseudo_verifiers2, &pseudo_verifiers1, &rekey_verifiers2, &rekey_verifiers1);
 
     assert!(rev_pseudonymized_dec.is_some());
-    assert_eq!(pseudo, rev_pseudonymized_dec.unwrap());
+    assert_eq!(pseudo_1, rev_pseudonymized_dec.unwrap());
 }
