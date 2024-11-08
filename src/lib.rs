eedc66b514a63dc95421501961fffe738b1b2fa2
Job Doesburg <job.doesburg@gmail.com>
2024-11-08 13:24:30 +0100

    Move proved to a different branch / module

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,6 @@ pub mod high_level {
 }
 pub mod distributed;
 
-pub mod zkps;
-pub mod proved;
-pub mod high_level_proved;
-pub mod verifiers_cache;
-pub mod distributed_proved;
 
 #[cfg(feature = "wasm")]
 mod wasm {
@@ -32,7 +27,6 @@ mod tests {
     mod primitives;
     mod high_level;
     mod distributed;
-    mod proved;
     #[cfg(feature = "legacy-pep-repo-compatible")]
     mod legacy_pep_repo;
 }
